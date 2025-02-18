1d72b66eb2c12cdfd1c1a69e964ad6ea935d1c7d
Bhesh Sejawal <bsejawal@gmail.com>
2025-02-18 11:39:15 -0600

    fixes

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,8 @@
 class Test {
 
-     private static int fibonacci(int n){
-         if(n<0) return 1;
-         if(n<=1){
-             return n;
-         }
-         return fibonacci(n-1) + fibonacci(n-2);
-     }
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 
 }
